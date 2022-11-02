ac7bb40fd24e26cf9f8f166a52bef09ebb83989e
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-11-02 10:25:13 -0300

    Funcionamento funcao distinct

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -2,43 +2,10 @@ package br.com.alura
 
 fun main() {
 
-    val banco = BancoDeNomes()
-    val nomesSalvo: Collection<String> = banco.nomes
+    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
+    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
 
-    banco.salva("Alex")
-    println(banco.nomes)
-    println(BancoDeNomes().nomes)
-    println(nomesSalvo)
+    println(assistiramAmbos.distinct())
 
-}
-
-class BancoDeNomes {
-
-    val nomes: Collection<String> get() = dados.toList()
-
-    fun salva(nome: String) {
-        dados.add(nome)
-    }
-
-    companion object {
-        private val dados = mutableListOf<String>()
-    }
-}
-
-fun testaColecao() {
-    val nomes: Collection<String> = listOf(
-        "Alex",
-        "Fran",
-        "Gui",
-        "Maria",
-        "Ana"
-    )
-
-    for (nome in nomes) {
-        println(nome)
-    }
-
-    println(nomes)
-    println("Tem o nome Alex? ${nomes.contains("Alex")}")
-    println("Tamanho da coleção: ${nomes.size}")
 }
