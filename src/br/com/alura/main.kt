efbbfc2bcb35938fce80404e9da202dcf24f9147
Pablo Henrique <ph.soft.servicos@gmail.com>
2022-11-02 17:32:25 -0300

    Outras funcionalidades com maps

--- src/br/com/alura/main.kt
+++ src/br/com/alura/main.kt
@@ -7,7 +7,7 @@ fun main() {
         Pair(2, 34.0),
         3 to 80.0,
         4 to 100.0,
-        5 to 150.0,
+        5 to 100.0,
         6 to 80.0
     )
 //    val valorPedido = pedidos.getValue(1)
@@ -46,7 +46,20 @@ fun main() {
     }
     println(pedidosPares)
 
+    println(pedidos + mapOf(7 to 90.0, 8 to 20.0))
+    println(pedidos)
 
+    println(pedidos - listOf(6, 5))
+    println(pedidos)
+
+    pedidos.keys.remove(1)
+    println(pedidos)
+
+    pedidos.values.remove(100.0)
+    println(pedidos)
+
+    pedidos -= 6
+    println(pedidos)
 }
 
 fun testaMap(pedidos: MutableMap<Int, Double>) {
